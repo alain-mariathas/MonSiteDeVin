44f6494c709010689d13ee6f6a407a1fb32a13bf
Alain Mariathas <a.mariathas@misigd.gsinfo-evry.net>
2017-05-22 15:05:36 +0200

    footer finie

--- footer.php
+++ footer.php
@@ -1,23 +1,7 @@
  <footer class="page-footer">
-          <div class="container">
-            <div class="row">
-              <div class="col l6 s12">
-                <h5 class="white-text">Footer Content</h5>
-                <p class="grey-text text-lighten-4"></p>
-              </div>
-              <div class="col l4 offset-l2 s12">
-                <h5 class="white-text">Liens</h5>
-                <ul>
-                  <li><a class="grey-text text-lighten-3" href="#!">Accueil</a></li>
-                  <li><a class="grey-text text-lighten-3" href="#!">Nos Vins</a></li>
-                  <li><a class="grey-text text-lighten-3" href="#!">Administration</a></li>
-                </ul>
-              </div>
-            </div>
-          </div>
           <div class="footer-copyright">
             <div class="container">
-            © 2017 Copyright Text
+              © 2017 - [webmaster@vin.fr] - L'abus d'alcool est angereux pour la santé, à consommer avec modération.
             </div>
           </div>
-        </footer>
+</footer>
