46ec1c9e38d86803779cbcd3a7b037d1ddc4d8b0
Alexandre Charpentier <alexandre.charpentier.ac@gmail.com>
2017-05-22 15:28:50 +0200

    header et head

---
+++ header.php
@@ -0,0 +1,21 @@
+
+  <div class="navbar-fixed">
+    <nav>
+      <div class="nav-wrapper">
+        <a href="#!" class="brand-logo">Logo</a>
+        <ul class="right hide-on-med-and-down">
+          <li style="width:1000px">SLOGAN slogan slogan slogan</li>
+          <li><a href="#">Catalogue de vins</a></li>
+          <li><a href="#">Fiche de vin</a></li>
+          
+          <?php
+          //insérer contrôle administrateur
+          ?>
+          <li><a href="#">Adminsitration</a></li>
+          <?php ?>
+          <li><a href="deconnexion.php"><i class="material-icons">power_settings_new</i></a></li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+        
--- index.php
+++ index.php
@@ -1,18 +1,8 @@
 <html>
-<head>
-
-<link type="text/css" rel="stylesheet" href="http://web.epnak.org/css/materialize.css"  media="screen,projection"/>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/css/materialize.min.css">
-<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
-<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-<link rel="stylesheet" href="css/style.css">
-<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/js/materialize.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/js/materialize.min.js"></script>
-</head>
-
+<?php include("head.php"); ?>
     <body>
-      <div class="container">
+    <?php include("header.php"); ?>
+      <div class="container main">
          Page Content goes here 
       </div>
   <?php include("footer.php"); ?>
