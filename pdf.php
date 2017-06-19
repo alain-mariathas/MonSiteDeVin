fdc7266ef3c82bdbde206bc99e933679d9e8a26a
Alexandre Charpentier <alexandre.charpentier.ac@gmail.com>
2017-06-19 14:43:55 +0200

    pdf terminé

--- pdf.php
+++ pdf.php
@@ -11,8 +11,7 @@ class PDF extends FPDF {
     function Header() {
         // Logo
         $this->Image('img/vin_card.jpg',8,2,40);
-        // Saut de ligne
-        $this->Ln(20);
+        $this->Ln(15);
     }
     // Footer
     function Footer() {
@@ -26,22 +25,24 @@ class PDF extends FPDF {
 $pdf = new PDF('P','mm','A4');
 $pdf->AddPage();
 $pdf->SetFont('Times','B',16);
-$pdf->Cell(40,10,'VINS DE l\'ASSOCIATION : ');
+$pdf->Cell(30,2,'MonSiteDeVin.fr');
+$pdf->Ln(20);
+$pdf->Cell(0,10,utf8_decode($_POST['nom_vin']),0,0,'C');
+$pdf->Ln(10);
 
 
-
-$reponse=$bdd->query('SELECT * FROM vins;');
 
 $hauteur=75;
 $pdf->SetFont('Times','B',12);
-while($donnee = $reponse->fetch())
-     {
+$marge=60;
+$pdf->Text($marge,$hauteur,utf8_decode($_POST['nom_vin']));
+$pdf->Text($marge,$hauteur+=12,utf8_decode($_POST['annee_vin']));
+$pdf->Text($marge,$hauteur+=12,utf8_decode($_POST['couleur_vin']));
+$pdf->Text($marge,$hauteur+=12,utf8_decode($_POST['region_vin']));
+$pdf->Text($marge,$hauteur+=12,utf8_decode($_POST['domaine_vin']));
+ $pdf->SetXY($marge,$hauteur+=12);
+$pdf->MultiCell(100,5,utf8_decode($_POST['description_vin']),0,'J',0,8);
 
-$pdf->Text(30,$hauteur,utf8_decode($donnee['vin_nom']));
-$pdf->Text(100,$hauteur,utf8_decode($donnee['vin_annee']));
-$pdf->Text(132,$hauteur,utf8_decode($donnee['vin_couleur']));
-         $hauteur+=8;
-     }
 
 $position_entete = 58;
 
@@ -50,14 +51,18 @@ function entete_table($position_entete){
     $pdf->SetDrawColor(183); // Couleur du fond
     $pdf->SetFillColor(221); // Couleur des filets
     $pdf->SetTextColor(0); // Couleur du texte
-    $pdf->SetY($position_entete);
-    $pdf->SetX(28);
-    $pdf->Cell(68,8,'Nom',1,0,'L',1);
-    $pdf->SetX(98);
-    $pdf->Cell(30,8,'Annee',1,0,'L',1);
-    $pdf->SetX(130); // 8 + 96
-    $pdf->Cell(30,8,'Couleur',1,0,'C',1);
-    $pdf->SetX(68); // 8 + 96
+    $pdf->SetY($position_entete+12);
+    $pdf->Cell(40,8,'Nom',1,0,'L',1);
+    $pdf->SetY($position_entete+24);
+    $pdf->Cell(40,8,'Annee',1,0,'L',1);
+    $pdf->SetY($position_entete+36);
+    $pdf->Cell(40,8,'Couleur',1,0,'L',1);
+    $pdf->SetY($position_entete+48);
+    $pdf->Cell(40,8,'Region',1,0,'L',1);
+    $pdf->SetY($position_entete+60);
+    $pdf->Cell(40,8,'Domaine',1,0,'L',1);
+    $pdf->SetY($position_entete+72);
+    $pdf->Cell(40,8,'Description',1,0,'L',1);
     $pdf->Ln(); // Retour à la ligne
 }
 entete_table($position_entete);
