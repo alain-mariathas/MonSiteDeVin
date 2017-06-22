b6fe816609a799fdb51d1414f0d4ad544464c752
Alain Mariathas <a.mariathas@misigd.gsinfo-evry.net>
2017-06-22 12:01:21 +0200

    Ajout termine mais pb avec le SELECT

--- AjoutVin.php
+++ AjoutVin.php
@@ -1,11 +1,14 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr">
-<?php include("head.php"); ?>
+<?php 
+include("head.php");
+include_once 'post_bdd_conn.php';
+ ?>
     <body>
     <?php include("header.php");
     //include("user_auth.php")
      ?>
-     
+     <form method="post" action="RecupDonnee_AjoutVin.php">
      <div class="container main">
      <div id="bloc_connexion" class="center-align" style="top:250px">
          <h3>Ajout de Vin</h3>
@@ -18,10 +21,15 @@
                   <input name="Name" id="VineNom" type="text">
                   <label for="VineNom">Nom</label>
                 </div>
-                <div class="input-field col s4">
-                  <input name="Color" id="VineCouleur" type="text">
-                  <label for="VineCouleur">Couleur</label>
-                </div>
+
+		<div class="input-field col s4">
+                	<select name="Couleur de vin" method="post">
+                    	     <option value="Rouge">Rouge</option>
+                             <option value="Blanc">Blanc</option>
+			     <option value="Rose">Rose</option>
+                 	</select>
+                 	<label for="VineCouleur">Couleur du vin</label>
+                 </div>
         </div>
         
         <div class="row">
@@ -32,27 +40,23 @@
         </div>
         
         <div class="row">
-                <div class="input-field col s4">
-                  <input name="DomaineName" id="VineDomaineNom" type="text">
-                  <label for="VineDomaineNom">Nom du Domaine</label>
-                </div>
-                
-                <div class="input-field col s4">
-                  <input name="DomaineAdresse" id="VineDomaineAdresse" type="text">
-                  <label for="VineDomaineAdresse">Adresse du Domaine</label>
-                </div>
-        </div>
-        
-        <div class="row">
-                <div class="input-field col s4">
-                  <input name="RegionName" id="VineRegion" type="text">
-                  <label for="VineRegion">Nom de la Region</label>
-                </div>
+		<div class="input-field col s12">
+		<select name="DomaineName" method="post">
+		<option value="" disabled selected>Choisissez le domaine</option>
+                          <?php
+                            $rep=$bdd->query('SELECT * FROM domaines;'); 
+                                while($donnees = $rep->fetch())
+                                    {
+                                        echo "<option value=\"".$donnees['dom_id']."\">".$donnees['dom_name']."</option>";
+                                    }
+                            $rep->closeCursor();
+			?>
+		</select>
+		</div>
         </div>
         
         
         <div class="row">
-        
                 <div class="input-field col s12">
                   <textarea id="DescriptionDuVin" class="materialize-textarea"></textarea>
                   <label for="DescriptionDuVin">Description du Vin</label>
--- RecupDonnee_AjoutVin.php
+++ RecupDonnee_AjoutVin.php
@@ -1,16 +1,10 @@
 <?php
-
-$req = $bdd->prepare('INSERT INTO domaines(dom_name, dom_adresse) VALUES(:VineDomaineNom, :VineDomaineAdresse)');
-
-$req->execute(array(
-
-    'dom_name' => $VineDomaineNom,
-
-    'dom_adresse' => $VineDomaineAdresse,
+include_once 'test_connexion.php';
+include_once 'post_bdd_conn.php';
 
 
-    ));
+$req=$bdd->prepare("INSERT INTO vins (vin_nom, vin_couleur, vin_annee, vin_description,domaine_id) VALUES (?,?,?,?,?);"); 
 
-
+$req->execute(array($_POST["Name"],$_POST["Color"],$_POST["Year"],$_POST["DescriptionDuVin"],$_POST["DomaineName"]));
 
 ?>
